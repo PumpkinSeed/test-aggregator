9f8b02f885d8040272ea91f83e48e0636871dc24
hajnalandor <andor.hajnal@fluidpay.com>
2020-07-16 16:18:33 +0200

    update error handling

--- server/src/mail.rs
+++ server/src/mail.rs
@@ -1,15 +1,23 @@
-// extern crate lettre;
-// extern crate lettre_email;
 use lettre::smtp::authentication::IntoCredentials;
 use lettre::{SmtpClient, Transport};
 use lettre_email::EmailBuilder;
 use std::env;
+use std::fmt;
 
 static USERNAME: &str = "anomaly.detector2020@gmail.com";
 static PASSWORD: &str = env!("PASSWORD","Missing password ENV");
 static SMTP_ADDRESS: &str = "smtp.gmail.com";
 
-pub fn send_mail(to: &str,subject: &str,text: &str) -> bool {
+#[derive(Debug, Clone)]
+pub struct MailError;
+
+impl fmt::Display for MailError {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+      write!(f, "Email not sent")
+  }
+}
+
+pub fn send_mail(to: &str,subject: &str,text: &str) -> Result<(),MailError> {
     let mut email = EmailBuilder::new()
         .to(to)
         .from(USERNAME)
@@ -18,14 +26,17 @@ pub fn send_mail(to: &str,subject: &str,text: &str) -> bool {
         .build();
        let email = match email {
           Ok(msg) => msg.into(),
-          Err(e) => return false
+          Err(e) => return Err(MailError)
         };
     let credentials = (USERNAME, PASSWORD).into_credentials();
     let mut client = SmtpClient::new_simple(SMTP_ADDRESS)
         .unwrap()
         .credentials(credentials)
         .transport();
-    client.send(email).is_ok()
+    match client.send(email).is_ok() {
+      true => return Ok(()),
+      false =>  return Err(MailError)
+    }
 }
 
 #[cfg(test)]
@@ -34,11 +45,15 @@ mod tests {
 
     #[test]
     fn send_mail_test() {
-        assert_eq!(send_mail("fineyoc131@djemail.net","empty subject","Since this is a simple and common operation, it would be convenient if it could be elided. Alas, because and_then is not sufficiently flexible, it cannot. However, we can instead use ?."),true);
+        send_mail("fineyoc131@djemail.net","empty subject","Since this is a simple and common operation, it would be convenient if it could be elided. Alas, because and_then is not sufficiently flexible, it cannot. However, we can instead use ?.").unwrap();
     }
 
     #[test]
-    fn send_mail_expect_false() {
-        assert_eq!(send_mail("wrong.email.net","empty subject","Since this is a simple and common operation, it would be convenient if it could be elided. Alas, because and_then is not sufficiently flexible, it cannot. However, we can instead use ?."),false);
+    fn send_mail_error() {
+        match send_mail("fineyoc13email.net","empty subject","Since this is a simple and common operation, it would be convenient if it could be elided. Alas, because and_then is not sufficiently flexible, it cannot. However, we can instead use ?.") {
+          Ok(()) => panic!(""),
+          Err(e) => ()
+        }
+
     }
 }
